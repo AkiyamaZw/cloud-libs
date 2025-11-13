9fcbaaa81afd04915b7a0a2e3f336ec35159df7b
Awei <awei@invercity.co>
2025-11-13 12:48:32 +0800

    add a SingletonSystem.h

--- src/runtime/application/private/graphics/renderer.cpp
+++ src/runtime/application/private/graphics/renderer.cpp
@@ -1,8 +1,15 @@
 #include "graphics/renderer.h"
 #include "graphics/vulkan/vulkan_device.h"
 
-namespace cloud
+namespace cloud::render
 {
+
+static Renderer g_renderer;
+
+Renderer* Renderer::Inst()
+{
+    return &g_renderer;
+}
 
 void CreateRenderer(GpuCreateParam &param) { vulkan::InitGpuDevice(param); }
 
--- src/runtime/application/public/graphics/renderer.h
+++ src/runtime/application/public/graphics/renderer.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "utility/SingletonSystem.h"
 #include "graphics/device.h"
 #include "data_structure/resource_manager.h"
 #include "vulkan/gpu_resource.h"
@@ -15,4 +16,21 @@ struct BufferResource: public Resource
 void CreateRenderer(GpuCreateParam &param);
 void DestroyRenderer();
 
+
+class Renderer: public SingletonSystem
+{
+public:
+    DEFINE_SINGLE_SYSTEM(Renderer);
+
+    void Init() override;
+
+    void Exit() override;
+
+    void BeginFrame();
+
+    void EndFrame();
+
+    void ResizeSwapChain(uint32_t width, uint32_t height);
+private:
+};
 } // namespace cloud
